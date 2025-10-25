89d270c96039622506dc3e4d96fc6cbaf16db78b
Faezeh Shakouri <f.shakori.com@gmail.com>
2025-10-25 12:10:06 +0200

    no need for amount

--- packages/contracts/src/Basket.sol
+++ packages/contracts/src/Basket.sol
@@ -3,42 +3,30 @@ pragma solidity ^0.8.28;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-interface IBasket {
-    struct TokenInfo {
-        uint256 percentage;
-        uint256 amount;
-    }
-
-    error ArraysLengthMismatch();
-    error PercentagesMustSumToExactly100();
-}
-
 /**
  * @title Basket
  * @notice A basket of tokens with a given percentage and amount
  */
-contract Basket is Ownable, IBasket {
+contract Basket is Ownable {
     address[] public tokens;
-    mapping(address token => TokenInfo info) public tokenInfo;
+    mapping(address token => uint256 percentage) public tokenPercentage;
+
+    error ArraysLengthMismatch();
+    error PercentagesMustSumToExactly100();
 
     constructor(
         address _owner,
         address[] memory _tokens,
         uint256[] memory _percentages
     ) Ownable(_owner) {
-        if (
-            _tokens.length != _percentages.length 
-        ) {
+        if (_tokens.length != _percentages.length) {
             revert ArraysLengthMismatch();
         }
 
         uint256 totalPercentage = 0;
         for (uint256 i = 0; i < _tokens.length; i++) {
             totalPercentage += _percentages[i];
-            tokenInfo[_tokens[i]] = TokenInfo({
-                percentage: _percentages[i],
-                amount: 0
-            });
+            tokenPercentage[_tokens[i]] = _percentages[i];
         }
 
         if (totalPercentage != 100) {
@@ -46,19 +34,24 @@ contract Basket is Ownable, IBasket {
         }
     }
 
-    function getTokensCount() public view returns (uint256) {
+    function getTokensLength() public view returns (uint256) {
         return tokens.length;
     }
 
-    function getAllTokens() public view returns (address[] memory) {
+    function getTokens() public view returns (address[] memory) {
         return tokens;
     }
 
-    function getAllTokenInfo() public view returns (TokenInfo[] memory) {
-        TokenInfo[] memory tokenInfos = new TokenInfo[](tokens.length);
+    function getTokensInfo()
+        public
+        view
+        returns (address[] memory _tokens, uint256[] memory _percentages)
+    {
+        _tokens = new address[](tokens.length);
+        _percentages = new uint256[](tokens.length);
         for (uint256 i = 0; i < tokens.length; i++) {
-            tokenInfos[i] = tokenInfo[tokens[i]];
+            _tokens[i] = tokens[i];
+            _percentages[i] = tokenPercentage[tokens[i]];
         }
-        return tokenInfos;
     }
 }
