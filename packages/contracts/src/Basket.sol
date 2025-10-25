5e4fa411aee7deabe0a212c96e683f0ff72d22bb
Faezeh Shakouri <f.shakori.com@gmail.com>
2025-10-25 15:29:03 +0200

    implement price feed integration using Pyth in Basket contract | update constructor to accept price feed IDs | add getTokenPrice function for fetching token prices

--- packages/contracts/src/Basket.sol
+++ packages/contracts/src/Basket.sol
@@ -2,24 +2,34 @@
 pragma solidity ^0.8.28;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import "@pythnetwork/pyth-sdk-solidity/IPyth.sol";
+import "@pythnetwork/pyth-sdk-solidity/PythStructs.sol";
+
+address constant SEPOLIA_PYTH_ADDRESS = 0xDd24F84d36BF92C65F92307595335bdFab5Bbd21;
 
 /**
  * @title Basket
  * @notice A basket of tokens with a given percentage and amount
  */
 contract Basket is Ownable {
+    IPyth public pyth;
     address[] public tokens;
     mapping(address token => uint256 percentage) public tokenPercentage;
+    mapping(address token => bytes32 priceFeedId) public tokenPriceFeedId;
 
     error ArraysLengthMismatch();
     error PercentagesMustSumToExactly100();
+    error PriceFeedIdNotFound();
+    error PriceNotFound();
 
     constructor(
         address _owner,
         address[] memory _tokens,
-        uint256[] memory _percentages
+        uint256[] memory _percentages,
+        bytes32[] memory _priceFeedIds
     ) Ownable(_owner) {
-        if (_tokens.length != _percentages.length) {
+        pyth = IPyth(SEPOLIA_PYTH_ADDRESS);
+        if (_tokens.length != _percentages.length || _tokens.length != _priceFeedIds.length) {
             revert ArraysLengthMismatch();
         }
         tokens = _tokens;
@@ -27,6 +37,7 @@ contract Basket is Ownable {
         for (uint256 i = 0; i < _tokens.length; i++) {
             totalPercentage += _percentages[i];
             tokenPercentage[_tokens[i]] = _percentages[i];
+            tokenPriceFeedId[_tokens[i]] = _priceFeedIds[i];
         }
 
         if (totalPercentage != 100) {
@@ -53,5 +64,21 @@ contract Basket is Ownable {
             _tokens[i] = tokens[i];
             _percentages[i] = tokenPercentage[tokens[i]];
         }
+    }
+
+    function getTokenPrice(address _token, bytes[] calldata priceUpdate) public payable returns (int64) {
+        bytes32 priceFeedId = tokenPriceFeedId[_token];
+        if (priceFeedId == bytes32(0)) {
+            revert PriceFeedIdNotFound();
+        }
+
+        uint256 fee = pyth.getUpdateFee(priceUpdate);
+        pyth.updatePriceFeeds{ value: fee }(priceUpdate);
+        PythStructs.Price memory price = pyth.getPriceNoOlderThan(priceFeedId, 60);
+
+        if (price.price == 0) {
+            revert PriceNotFound();
+        }
+        return price.price;
     }
 }
